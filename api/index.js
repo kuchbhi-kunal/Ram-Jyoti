b00b36dbabc5f7500f9eff8a423d0fde3de079a8
ParthSankpal <parth.sankpal47@gmail.com>
2024-01-21 16:18:05 +0530

    Ready for deployment "IG"

--- api/index.js
+++ api/index.js
@@ -9,6 +9,10 @@ import bodyParser from "body-parser";
 
 import userRouter from "./routes/user.route.js"
 
+import path from 'path';
+
+const __dirname = path.resolve();
+
 
 mongoose
   .connect(process.env.MONGODB)
@@ -29,8 +33,20 @@ app.use(express.static('jaisiyaram'));
 
 app.use('/api/user', userRouter);
 
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+// // console.log(__dirname + '/client/dist', "DIRECTORY PARTH");
 
-app.listen(3000, () => {
-  console.log("Server is runnung on port 3000");
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// })
+
+app.use(express.static(path.join(__dirname, 'jaisiyaram')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'jaisiyaram', 'index.html'));
 });
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
